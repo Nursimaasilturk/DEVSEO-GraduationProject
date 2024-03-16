a0b0c5c3f5f942730b0e2a5ebd4b8c06d1b12068
Omer Atayilmaz <omeratayilmaz@Omers-MacBook-Pro.local>
2024-03-16 18:26:39 +0300

    Code review part 2

---
+++ core/openAPI.js
@@ -0,0 +1,31 @@
+const OpenAI = require('openai');
+let { chatGptAPIKey } = require('./../env/credentials.js');
+
+module.exports = async function openAIApi(pageContent){
+    const oaInstance = new OpenAI.OpenAI({
+        apiKey: chatGptAPIKey
+    });
+    let answerRules = `1- Answer must be english, 2- Detect the SEO Problems and give advices, 3- Advices must be as an ordered list.(No humanly conversation only answer please!) Here is content:`
+	console.log(answerRules + pageContent);
+
+	try {
+        const response = await oaInstance.chat.completions.create({
+            model: 'gpt-3.5-turbo', 
+            messages: [{
+                role: "system",
+                content: "You are a helpful assistant."
+            },{
+                role: "user",
+                content: answerRules + pageContent
+            }]
+        });
+	
+
+        // Process and return the response
+        console.log(response); 
+        return response;
+    } catch (error) {
+        console.error(error); // Handle any errors
+        throw error; // Rethrow the error for further handling if needed
+    }
+}
--- extension.js
+++ extension.js
@@ -1,5 +1,3 @@
-const OpenAI = require('openai');
-let { chatGptAPIKey } = require('./env/credentials.js');
 const vscode = require('vscode');
 const StatusBar = require('./core/StatusBar.js');
 const Checker = require('./core/checker.js');
@@ -13,37 +11,6 @@ async function activate(context) {
 	context.subscriptions.push(Checker.singlePageSEOChecker);
 	context.subscriptions.push(Checker.allPagesSEOChecker);
 }
-
-
-async function openAIApi(pageContent){
-    const oaInstance = new OpenAI.OpenAI({
-        apiKey: chatGptAPIKey
-    });
-    let answerRules = `1- Answer must be english, 2- Detect the SEO Problems and give advices, 3- Advices must be as an ordered list.(No humanly conversation only answer please!) Here is content:`
-	console.log(answerRules + pageContent);
-
-	try {
-        const response = await oaInstance.chat.completions.create({
-            model: 'gpt-3.5-turbo', 
-            messages: [{
-                role: "system",
-                content: "You are a helpful assistant."
-            },{
-                role: "user",
-                content: answerRules + pageContent
-            }]
-        });
-	
-
-        // Process and return the response
-        console.log(response); 
-        return response;
-    } catch (error) {
-        console.error(error); // Handle any errors
-        throw error; // Rethrow the error for further handling if needed
-    }
-}
-
 
 // This method is called when your extension is deactivated
 function deactivate() {}
